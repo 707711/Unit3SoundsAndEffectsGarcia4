1526cf243768423ac16a0b257b3c6642e46810a2
707711 <707711@student.dusd.net>
2023-03-24 11:41:11 -0700

    particle effects and animation
    running and death animation, and explosion of smoke then game is over

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,22 +10,28 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
 
     public bool isOnground = true;
-    public bool gameOver;
+    public bool gameOver = false;
+
+    private Animator playerAnim;
+
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnground)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnground && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnground = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -39,6 +46,9 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
     }
 
